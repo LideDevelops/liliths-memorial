07e56ca41a772d555703a37c0875337f5cbdef69
Yannick Großkreutz <grosskreutz.yannick@gmail.com>
2024-07-18 11:35:57 +0200

    Started with edit page, only accessable via login

---
+++ liliths-memorial-client/src/app/edit/page.tsx
@@ -0,0 +1,10 @@
+import { SignOut } from "@/components/sign.out";
+
+export default function Edit() {
+    return <div>
+        <h1>Edit</h1>
+        <p>This is the edit page.</p>
+        <p>TODO: Add options to edit pages</p>
+        <SignOut></SignOut>
+    </div>
+  }
--- liliths-memorial-client/src/middleware.ts
+++ liliths-memorial-client/src/middleware.ts
@@ -1,1 +1,11 @@
-export { auth as middleware } from "@/auth"
+import type { NextRequest } from 'next/server'
+import { auth } from './auth';
+ 
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+ 
+  if (!session?.user && request.url.match('/edit')) {
+    return Response.redirect(new URL('/', request.url))
+  }
+}
+ 
